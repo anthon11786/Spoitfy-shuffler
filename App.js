5e89f61d8543aee7170abf476002c7e141f68cf6
Anthony <Anthony@unknown.com>
2019-07-27 22:16:01 -0400

    Cleaning up

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, TextInput, View, Switch } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Switch, Alert } from 'react-native';
 
 export default class Clock extends Component {
   constructor(props) {
@@ -11,36 +11,71 @@ export default class Clock extends Component {
     }
   }
 
-  toggleSwitch = (value) =>{
-    this.setState({pm:value})
+  toggleSwitch = value =>{
+    this.setState({pm:value});
   }
+
+  hourSet = text =>{
+    numbers = new Set(['','1','2','3','4','5','6','7','8','9','10','11','12']);
+    if(numbers.has(text)){
+      this.setState({hour:text});
+    }
+    else {
+      Alert.alert(
+        'Invalid Hour Time',
+        'Enter a number between 1 and 12.');
+    }
+  }
+
+  minuteSet = text =>{
+    var re = /[0-5][0-9]/;
+    if(text.length == 2 && re.test(text) || text.length<2){
+      this.setState({minute:text});
+    }else{
+      Alert.alert(
+        'Invalid Minute Time',
+        'Enter a number between 0 and 59.');
+      this.setState({minute:''});
+    }
+  }
+
+
 
   render(){
     return (
       <View style={styles.container}>
-        <Text>Enter alarm time</Text>
+        <Text style={{fontSize:30, fontStyle:'italic'}} >Enter alarm time</Text>
         <View style={styles.timeContainer}>
           <TextInput
             textAlignVertical='top'
+            keyboardType = 'numeric'
+            fontSize='30'
             placeholder="0h"
-            onChangeText={(text) => this.setState({hour:text})}
+            onChangeText={(text) => this.hourSet(text)}
             value={this.state.hour}
           />
-          <Text> : </Text>
+          <Text style = {{fontSize:30}}> : </Text>
           <TextInput
             textAlignVertical='top'
+            keyboardType = 'numeric'
+            fontSize='30'
             placeholder="0m"
-            onChangeText={(text) => this.setState({minute:text})}
+            onChangeText={(text) => this.minuteSet(text)}
             value={this.state.minute}
           />
         </View>
         <View style={styles.timeContainer}>
-          <Text textAlignVertical='bottom'>AM</Text>
+          <Text textAlign='right' style={{color:'red'}}>AM</Text>
           <Switch
             onValueChange = {this.toggleSwitch}
             value = {this.state.pm}
+            trackColor = {{
+              false: 'red',
+              true: 'blue'
+            } }
+            ios_backgroundColor = 'red'
           />
-          <Text textAlignVertical='top'>PM</Text>
+          <Text textAlignVertical='center' style={{color:'blue'}}>PM</Text>
         </View>
       </View>
     );
@@ -52,11 +87,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
   timeContainer:{
     flexDirection:'row',
     justifyContent:'center'
   },
+  AMPMContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems:'flex-end',
+    justifyContent: 'space-between',
+    fontSize:30,
+
+
+  }
 
 });
